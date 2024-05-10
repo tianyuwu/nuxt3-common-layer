c5a43bec2f4f9bb31ea116a297fc208a557453f0
whitney <iwhitney@163.com>
2024-05-10 13:05:24 +0800

    feat: add modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: [
+    '@unocss/nuxt',
+    '@vueuse/nuxt',
+    '@pinia/nuxt',
+    '@nuxt/eslint',
+    '@element-plus/nuxt'
+  ],
+  css: [
+    '@unocss/reset/tailwind.css',
+  ]
 })
